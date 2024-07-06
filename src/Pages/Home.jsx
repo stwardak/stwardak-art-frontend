f904a848a7bbe8aef81993ebf2c674147bd1fb31
stwardak <148924249+stwardak@users.noreply.github.com>
2024-07-06 18:43:00 -0500

    add header, footer, home page, and about page skeletons

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <main>
+        <div>
+          <p>
+            This is the home page.
+          </p>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default Home;
+
---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+function Header() {
+
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
